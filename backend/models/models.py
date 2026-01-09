54d8ad93d76d15023a9684acc841291bc0c1937f
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-09 12:19:53 +0530

    Created the another taable named contact with 4 columns in it

--- backend/models/models.py
+++ backend/models/models.py
@@ -10,3 +10,11 @@ class Project(Base):
     tech_stack = Column(String(300))
     github_url = Column(String(300))
     live_url = Column(String(300), nullable=True)
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(200), nullable=False)
+    message = Column(Text, nullable=False)
