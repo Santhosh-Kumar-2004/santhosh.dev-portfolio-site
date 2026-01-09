7cd2ed35f8c3b3be236e8beac4d7c15e75293254
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-09 12:17:46 +0530

    Added a taable named Project and with some of the colukms

---
+++ backend/models/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text
+from core.database import Base
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(200), nullable=False)
+    description = Column(Text)
+    tech_stack = Column(String(300))
+    github_url = Column(String(300))
+    live_url = Column(String(300))
