13feab88cfb05d04ab82e76808fe465bdd4b0dd9
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-09 12:22:16 +0530

    Updating the main.py filee and created the fastapi update dinstance

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 app = FastAPI(title="Santhosh Portfolio API")
+
+load_dotenv()
+
+app = FastAPI(
+    title="OTP Auth Verification System(MVP)",
+    description="A simple full-stack authentication MVP using OTP (One-Time Password) instead of passwords.",
+    version="0.7.0",   
+)
 
 @app.get("/api/health")
 def health_check():
