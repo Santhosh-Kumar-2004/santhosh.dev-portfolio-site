51dd123ae3a1f9db69eb8493d3d5298613e33da5
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-05 17:39:36 +0530

    Updated the background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,17 +1,30 @@
 export default function Background() {
   return (
     <div className="app-background">
-      {/* Grid */}
+      {/* The base architectural grid */}
       <div className="grid-layer" />
+      
+      {/* Subtle vignettes to focus the eye on the center */}
+      <div className="vignette-overlay" />
 
-      {/* Vertical neon flows */}
-      <div className="neon-line vertical v1" />
-      <div className="neon-line vertical v2" />
-      <div className="neon-line vertical v3" />
+      {/* Vertical neon "data" flows */}
+      <div className="neon-line-container">
+        <div className="neon-line vertical v1" />
+        <div className="neon-line vertical v2" />
+        <div className="neon-line vertical v3" />
+        <div className="neon-line vertical v4" />
+      </div>
 
-      {/* Horizontal neon flows */}
-      <div className="neon-line horizontal h1" />
-      <div className="neon-line horizontal h2" />
+      {/* Horizontal neon "data" flows */}
+      <div className="neon-line-container">
+        <div className="neon-line horizontal h1" />
+        <div className="neon-line horizontal h2" />
+        <div className="neon-line horizontal h3" />
+      </div>
+      
+      {/* Pulsing ambient orbs for depth */}
+      <div className="ambient-orb orb-1" />
+      <div className="ambient-orb orb-2" />
     </div>
   );
 }
