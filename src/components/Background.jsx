ffb85d9186c4c92aa93fa2ea04735d4c4b71c650
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-05 17:25:42 +0530

    Created the backengroundjsx file

---
+++ src/components/Background.jsx
@@ -0,0 +1,9 @@
+export default function Background() {
+  return (
+    <div className="app-background">
+      <div className="grid-layer" />
+      <div className="neon-line neon-line-1" />
+      <div className="neon-line neon-line-2" />
+    </div>
+  );
+}
