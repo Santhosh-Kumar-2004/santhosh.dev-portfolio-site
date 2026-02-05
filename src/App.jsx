46522170c0c634603297104ceda8ef91ee551ce0
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-05 12:50:46 +0530

    Updted the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Skills from "./components/Skills";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Pages will be added one by one */}
-      </Routes>
-    </BrowserRouter>
+    <>
+      {/* <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact /> */}
+    </>
   );
 }
 
