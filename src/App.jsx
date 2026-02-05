5754b9f0937a1f413fb65cbe5dc8009b5c55d023
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-05 16:56:09 +0530

    Creating the magnetic button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import DraggableID from "./components/DraggableID";
 import ScrollProgress from "./components/ScrollProgress";
 import About from "./components/About";
+import CustomCursor from "./components/CustomCursor";
 // import Skills from "./components/Skills";
 // import Projects from "./components/Projects";
 // import Contact from "./components/Contact";
@@ -12,6 +13,7 @@ function App() {
     <>
       <DraggableID />
       <ScrollProgress />
+      <CustomCursor />
       <Navbar />
       <Hero />
       <About />
