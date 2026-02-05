ede9a5f54bf00440647ac4580e752257aa97d200
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-05 12:23:53 +0530

    Updated the app.jsx file with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Pages will be added one by one */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
